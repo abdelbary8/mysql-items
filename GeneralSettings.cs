2aa425b6af34a32e8e4854a1c7eee09ecf332751
MOHAMED abdelbary <abdelbary8@yahoo.com>
2016-01-23 23:20:52 +0400

    General Settings

---
+++ GeneralSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLbase
+{
+    public class GeneralSettings
+    {
+        public static string ConnectionString {
+            get {
+                return "Server=localhost;Database=db;Uid=user;Pwd=pass;";
+            }
+        }
+
+    }
+}
